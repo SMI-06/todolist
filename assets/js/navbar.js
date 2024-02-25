61246f71fea1e1809ae1f3e9a228f9431b1c6bbd
Syed Muhammad Ibraheem <syedmuhammadibraheem06@gmail.com>
2024-02-26 02:04:54 +0500

    2nd

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,6 +1,8 @@
 let navbar = `<nav class="navbar navbar-expand-lg bg-info">
 <div class="container-fluid">
-    <a class="navbar-brand text-light" href="#">To Do List</a>
+    <a class="navbar-brand text-light" href="#">
+    <img src="assets/images/Logo.png" width="80px" height="50px" alt="logo">
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
         data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
         aria-label="Toggle navigation">
@@ -9,15 +11,15 @@ let navbar = `<nav class="navbar navbar-expand-lg bg-info">
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 gap-2">
             <li class="nav-item">
-                <a class="nav-link text-light active" aria-current="page" href="./index.html">Home</a>
+                <a class="nav-link text-light active" aria-current="page" href="./index.html"><i class="bi bi-house-heart"></i> Home</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-light active" aria-current="page" href="#">About</a>
+                <a class="nav-link text-light active" aria-current="page" href="#"><i class="bi bi-exclamation-circle"></i> About</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link text-light active" aria-current="page" href="#">Contact</a>
+                <a class="nav-link text-light active" aria-current="page" href="#"><i class="bi bi-phone-vibrate"></i> Contact</a>
             </li>
-            <div id="isUserLogedIn">
+            <div id="login">
                 <a href="login.html" type="button" class="btn btn-light">Login </a>
             </div>
         </ul>
@@ -25,6 +27,9 @@ let navbar = `<nav class="navbar navbar-expand-lg bg-info">
     </div>
 </div>
 </nav>`;
+
+
+
 
 let navbarDiv = document.getElementById("navbar");
 navbarDiv.innerHTML = navbar;
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
 
+let loginbtn = document.getElementById('login')
 
-let btnRegister = document.getElementById("btn-register")
+// 
+window.addEventListener("load", () => {
+    if (sessionStorage.getItem("currentUser")) {
+        loginbtn.innerHTML = ""
+        let userLogin = JSON.parse(sessionStorage.getItem("currentUser"));
+        loginbtn.innerHTML =
+            `
+            <div class="btn-group">
+        <a class="btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false" href="#"><i class="bi bi-person-circle"></i> ${userLogin.name}</a>
+        <ul class="dropdown-menu" style="position: absolute; left:-30px">
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><a class="dropdown-item" href="#">Setting</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item" onclick="Logout()" href="#">Logout</a></li>
+        </ul>
+        </div>
+        `
+    }
+})
 
-btnRegister.addEventListener("click", () => {
+function register() {
     let userName = document.getElementById("userName").value
     let userEmail = document.getElementById("email").value
     let userPassword = document.getElementById("password").value
@@ -13,12 +32,92 @@ btnRegister.addEventListener("click", () => {
             // text: "That thing is still around?",
             icon: "question"
         });
-    }else{
-        if(localStorage.getItem("userDetails")){
-            
+    } else {
+        let users = {
+            name: userName,
+            Email: userEmail,
+            password: userPassword,
+        }
+        $addDetail = localStorage.setItem("userData", JSON.stringify([users]))
+        $login = sessionStorage.setItem("currentUser", JSON.stringify(users))
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Register SuccessFully",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        setTimeout(function () {
+            location.href = 'index.html';
+        }, 1501);
+    }
+}
+
+
+//  Add Task Function
+function addtask() {
+    if (!sessionStorage.getItem("currentUser")) {
+        Swal.fire("Please Log In");
+        document.getElementById('showtasks').innerHTML = ""
+    } else {
+        let addtask = document.getElementById('taskName').value
+        let taskdate = document.getElementById('taskdate').value
+
+        if (!(addtask && taskdate)) {
+            Swal.fire({
+                title: "Fill The All Record?",
+                // text: "That thing is still around?",
+                icon: "question"
+            });
+        } else {
+            let currentUser = JSON.parse(sessionStorage.getItem("currentUser"));
+            let todolists = {
+                userName: currentUser.name,
+                taskName: addtask,
+                taskDate: taskdate,
+            }
+            if (localStorage.getItem("todoLists")) {
+                let tasks = JSON.parse(localStorage.getItem('todoLists'));
+                tasks.push(todolists);
+                localStorage.setItem("todoLists", JSON.stringify(tasks));
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Task Added SuccessFull",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                setTimeout(function () {
+                    window.location.reload();
+                }, 1501);
+            } else {
+                $addDetail = localStorage.setItem("todoLists", JSON.stringify([todolists]))
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: "Task Added SuccessFull",
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                setTimeout(function () {
+                    window.location.reload();
+                }, 1501);
+            }
         }
     }
+}
 
-})
-{/* <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> */}
+//  Logout Function
+function Logout() {
+    Swal.fire("Warning", "Are you sure want to logout?", "question").then(
+        (res) => {
+            if (res.isConfirmed) {
+                sessionStorage.removeItem("currentUser");
+                location.href = "./index.html";
+            }
+        }
+    );
+}
+
+{/* <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> */ }
 
